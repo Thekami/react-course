66a517e96d8d17bf00ff1777aa85b1618498ba88
Angel Torres <at.lopez@outlook.com>
2023-02-06 21:41:33 -0600

    primer commit

---
+++ src/Start.js
@@ -0,0 +1,49 @@
+export function Usuario(params) {
+  return (
+    <>
+      <h1>
+        {params.nombre} {params.apellido}
+      </h1>
+      <p>Saldo: ${params.Saldo}</p>
+      <p>Dirección:</p>
+      <ul>
+        <li>Estado: {params.direccion.estado}</li>
+        <li>Municipio: {params.direccion.municipio}</li>
+        <li>Calle: {params.direccion.Calle}</li>
+        <li>Numero interior: {params.direccion.noint}</li>
+        <li>Colonia: {params.direccion.colonia}</li>
+        <li>Código Postal: {params.direccion.cp}</li>
+      </ul>
+      <p>Suma de puntos: {Sumar(params.puntos)}</p>
+    </>
+  );
+}
+
+export function OtroUsuario({ data }) {
+  return (
+    <>
+      <h1>
+        {data.nombre} {data.apellido}
+      </h1>
+      <p>Saldo: ${data.Saldo}</p>
+      <p>Dirección:</p>
+      <ul>
+        <li>Estado: {data.direccion.estado}</li>
+        <li>Municipio: {data.direccion.municipio}</li>
+        <li>Calle: {data.direccion.Calle}</li>
+        <li>Numero interior: {data.direccion.noint}</li>
+        <li>Colonia: {data.direccion.colonia}</li>
+        <li>Código Postal: {data.direccion.cp}</li>
+      </ul>
+      <p>Suma de puntos: {Sumar(data.puntos)}</p>
+    </>
+  );
+}
+
+export function Sumar(params) {
+  var suma = 0;
+  for (let i = 0; i < params.length; i++) {
+    suma += params[i];
+  }
+  return suma;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Usuario, OtroUsuario } from "./Start";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+var data = [
+  {
+    nombre: "Juan",
+    apellido: "Martinez",
+    Saldo: 1500,
+    puntos: [98.5, 25, 45],
+    direccion: {
+      estado: "Colima",
+      municipio: "Villa de Álvarez",
+      Calle: "Dr. Miguel Galindo",
+      noint: "75",
+      colonia: "Centro",
+      cp: "28000",
+    },
+  },
+  {
+    nombre: "Angel",
+    apellido: "Torres",
+    Saldo: 5000,
+    puntos: [10.5, 105, 78.3],
+    direccion: {
+      estado: "Colima",
+      municipio: "Villa de Álvare",
+      Calle: "Fasto Macias Andrade",
+      noint: "1124",
+      colonia: "Senderos de bumgambilias",
+      cp: "28975",
+    },
+  },
+];
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+    <OtroUsuario data={data[0]} />
+    <OtroUsuario data={data[1]} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <br></br>
+    <p>------------------------------------------------</p>
+    <br></br>
+
+    <Usuario
+      nombre="Juan"
+      apellido="Martinez"
+      Saldo={1500}
+      puntos={[98.5, 25, 45]}
+      direccion={{
+        estado: "Colima",
+        municipio: "Villa de Álvarez",
+        Calle: "Dr. Miguel Galindo",
+        noint: "75",
+        colonia: "Centro",
+        cp: "28000",
+      }}
+    />
+    <Usuario
+      nombre="Angel"
+      apellido="Torres"
+      Saldo={5000}
+      puntos={[10.5, 105, 78.3]}
+      direccion={{
+        estado: "Colima",
+        municipio: "Colima",
+        Calle: "Fasto Macias Andrade",
+        noint: "1124",
+        colonia: "Senderos de bumgambilias",
+        cp: "28975",
+      }}
+    />
+  </>
+);
